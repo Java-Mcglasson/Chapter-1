9cc64094f9e64ac5d697011ef923f1a32a6d13cd
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2019-08-28 08:37:40 -0500

    update

---
+++ src/TablesAnChairs.java
@@ -0,0 +1,23 @@
+public class TablesAnChairs {
+
+    public static void main(String[] args){
+        System.out.println("|                                   | ");
+        System.out.println("|           ______________          | ");
+        System.out.println("---------   |            |   -------- ");
+        System.out.println("|       |   |            |   |      | ");
+        System.out.println("|       |   |            |   |      | ");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/TacoTruck.java
@@ -0,0 +1,22 @@
+public class TacoTruck {
+
+    public static void main(String[] args){
+
+        System.out.println("|----------------------------------------------------------|                          ");
+        System.out.println("|                                                           |                         ");
+        System.out.println("|            -------  ________  ---------   ---------        |                        ");
+        System.out.println("|               |     |      |  |           |       |         |                       ");
+        System.out.println("|               |     |------|  |           |       |          |                      ");
+        System.out.println("|               |     |      |  |           |       |           |________________     ");
+        System.out.println("|               |     |      |  ---------   ---------                            |    ");
+        System.out.println("|                                                                             ----    ");
+        System.out.println("|           _______                                      _______              |       ");
+        System.out.println("|          | OOOOO |                                    | OOOOO |             ----    ");
+        System.out.println("|         | OOOOOOO |                                  | OOOOOOO |               |    ");
+        System.out.println("|        | OOOOOOOOO |                                | OOOOOOOOO |              |    ");
+        System.out.println("|-------  OOOOOOOOOOO  ------------------------------  OOOOOOOOOOO  --------------    ");
+        System.out.println("            OOOOOOO                                     OOOOOOOOO                     ");
+
+    }
+
+}
